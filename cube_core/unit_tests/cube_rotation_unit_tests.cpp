8c384921c9f0c08b6669ed64c9213c22ac4e4a39
Himanshu Gupta <44716652+hg387@users.noreply.github.com>
2024-07-30 14:44:36 +0000

    - Linted code - Updated README

--- cube_core/unit_tests/cube_rotation_unit_tests.cpp
+++ cube_core/unit_tests/cube_rotation_unit_tests.cpp
@@ -1,91 +1,96 @@
 #include "gtest/gtest.h"
-#include "../face.h"
 #include "../face.cpp"
 
+class RotationTest : public testing::Test
+{
+protected:
+    RotationTest()
+    {
+    }
 
-class RotationTest: public testing::Test{
-    protected:
-        RotationTest(){
+    U<3> U{};
+    D<3> D{};
+    F<3> F{};
+    B<3> B{};
+    L<3> L{};
+    R<3> R{};
 
-        }
-
-        U<3> U{};
-        D<3> D{};
-        F<3> F{};
-        B<3> B{};
-        L<3> L{};
-        R<3> R{};
-
-        ::U<3> UInit{};
-        ::D<3> DInit{};
-        ::F<3> FInit{};
-        ::B<3> BInit{};
-        ::L<3> LInit{};
-        ::R<3> RInit{};
+    ::U<3> UInit{};
+    ::D<3> DInit{};
+    ::F<3> FInit{};
+    ::B<3> BInit{};
+    ::L<3> LInit{};
+    ::R<3> RInit{};
 };
 
-TEST_F(RotationTest, UpRotationTest) {
+TEST_F(RotationTest, UpRotationTest)
+{
     U.rotateClockwise(B, R, F, L);
     U.rotateAntiClockwise(B, R, F, L);
-    EXPECT_TRUE(UInit==U);
-    EXPECT_TRUE(LInit==L);
-    EXPECT_TRUE(FInit==F);
-    EXPECT_TRUE(RInit==R);
-    EXPECT_TRUE(BInit==B);
-    EXPECT_TRUE(DInit==D);
+    EXPECT_TRUE(UInit == U);
+    EXPECT_TRUE(LInit == L);
+    EXPECT_TRUE(FInit == F);
+    EXPECT_TRUE(RInit == R);
+    EXPECT_TRUE(BInit == B);
+    EXPECT_TRUE(DInit == D);
 }
 
-TEST_F(RotationTest, DownRotationTest) {
+TEST_F(RotationTest, DownRotationTest)
+{
     D.rotateClockwise(F, R, B, L);
     D.rotateAntiClockwise(F, R, B, L);
-    EXPECT_TRUE(UInit==U);
-    EXPECT_TRUE(LInit==L);
-    EXPECT_TRUE(FInit==F);
-    EXPECT_TRUE(RInit==R);
-    EXPECT_TRUE(BInit==B);
-    EXPECT_TRUE(DInit==D);
+    EXPECT_TRUE(UInit == U);
+    EXPECT_TRUE(LInit == L);
+    EXPECT_TRUE(FInit == F);
+    EXPECT_TRUE(RInit == R);
+    EXPECT_TRUE(BInit == B);
+    EXPECT_TRUE(DInit == D);
 }
 
-TEST_F(RotationTest, FrontRotationTest) {
+TEST_F(RotationTest, FrontRotationTest)
+{
     F.rotateClockwise(U, R, D, L);
     F.rotateAntiClockwise(U, R, D, L);
-    EXPECT_TRUE(UInit==U);
-    EXPECT_TRUE(LInit==L);
-    EXPECT_TRUE(FInit==F);
-    EXPECT_TRUE(RInit==R);
-    EXPECT_TRUE(BInit==B);
-    EXPECT_TRUE(DInit==D);
+    EXPECT_TRUE(UInit == U);
+    EXPECT_TRUE(LInit == L);
+    EXPECT_TRUE(FInit == F);
+    EXPECT_TRUE(RInit == R);
+    EXPECT_TRUE(BInit == B);
+    EXPECT_TRUE(DInit == D);
 }
 
-TEST_F(RotationTest, LeftRotationTest) {
+TEST_F(RotationTest, LeftRotationTest)
+{
     L.rotateClockwise(U, F, D, B);
     L.rotateAntiClockwise(U, F, D, B);
-    EXPECT_TRUE(UInit==U);
-    EXPECT_TRUE(LInit==L);
-    EXPECT_TRUE(FInit==F);
-    EXPECT_TRUE(RInit==R);
-    EXPECT_TRUE(BInit==B);
-    EXPECT_TRUE(DInit==D);
+    EXPECT_TRUE(UInit == U);
+    EXPECT_TRUE(LInit == L);
+    EXPECT_TRUE(FInit == F);
+    EXPECT_TRUE(RInit == R);
+    EXPECT_TRUE(BInit == B);
+    EXPECT_TRUE(DInit == D);
 }
 
-TEST_F(RotationTest, RightRotationTest) {
+TEST_F(RotationTest, RightRotationTest)
+{
     R.rotateClockwise(U, B, D, F);
     R.rotateAntiClockwise(U, B, D, F);
-    EXPECT_TRUE(UInit==U);
-    EXPECT_TRUE(LInit==L);
-    EXPECT_TRUE(FInit==F);
-    EXPECT_TRUE(RInit==R);
-    EXPECT_TRUE(BInit==B);
-    EXPECT_TRUE(DInit==D);
+    EXPECT_TRUE(UInit == U);
+    EXPECT_TRUE(LInit == L);
+    EXPECT_TRUE(FInit == F);
+    EXPECT_TRUE(RInit == R);
+    EXPECT_TRUE(BInit == B);
+    EXPECT_TRUE(DInit == D);
 }
 
-TEST_F(RotationTest, BackRotationTest) {
+TEST_F(RotationTest, BackRotationTest)
+{
     B.rotateClockwise(U, L, D, R);
     B.rotateAntiClockwise(U, L, D, R);
-    EXPECT_TRUE(UInit==U);
-    EXPECT_TRUE(LInit==L);
-    EXPECT_TRUE(FInit==F);
-    EXPECT_TRUE(RInit==R);
-    EXPECT_TRUE(BInit==B);
-    EXPECT_TRUE(DInit==D);
+    EXPECT_TRUE(UInit == U);
+    EXPECT_TRUE(LInit == L);
+    EXPECT_TRUE(FInit == F);
+    EXPECT_TRUE(RInit == R);
+    EXPECT_TRUE(BInit == B);
+    EXPECT_TRUE(DInit == D);
 }
